07cacc6e3da1f39995ecadf3f1acd10eca5601e0
Pablo Saba <sabaliauskaspablo@gmail.com>
2021-04-18 10:19:09 -0300

    end tp2

---
+++ TP3_ElJuegoDeLaVida/practica/code/tata.py
@@ -0,0 +1,7 @@
+test = 'ABPPDKKFTATAGANETATALLASFDKLDFEERLOWENNTRETERT'
+
+
+def searchPromoter(adnSecuence):
+	sec = adnSecuence[(adnSecuence.find('TATA',5) + 4):]
+	sec = sec[:(sec.find('TATA'))]
+	return sec
